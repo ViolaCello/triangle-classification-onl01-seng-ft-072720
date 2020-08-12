a92abfcb26bf28c4666fcad932bdd9bb887709b5
Anthony Lepore <violacellonyc@gmail.com>
2020-08-12 19:04:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,19 @@
 class Triangle
   # write code here
   
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(s,ss,sss)
+    @side1 = s 
+    @side2 = ss 
+    @side3 = sss 
+  end
   
   
   
   
+  class TriangleError < StandardError
+    # triangle error code
+  end
   
 end
